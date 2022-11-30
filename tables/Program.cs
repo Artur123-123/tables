79804e42de475096a10db6b135788dc1ed065a3b
uczen <uczen@TE-PIO-D-21S307>
2022-11-30 15:13:35 +0100

    Dodaj pliki projektów.

---
+++ tables/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tables
+{
+    internal class Program
+    {
+        class Tablice
+        {        
+            internal int[] numbers = new int[5];
+
+            internal void wpisywanie()
+            {
+                for(int i = 0; i < numbers.Length; i++)
+                {
+                    numbers[i] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+
+            internal string wypisywanie()
+            {
+                string napis = "";
+                foreach(int w in numbers)
+                {
+                    napis = w.ToString() + "; ";
+                    Console.Write(napis);
+                }
+                return napis;
+            }
+
+            internal int mat(string znak)
+            {
+                znak = Console.ReadLine();
+                int suma = numbers[0];
+                if (znak == "+")
+                {
+                    foreach (int w in numbers)
+                    {
+                        suma = suma + w;
+                    }
+                }
+                else if (znak == "*")
+                {
+                    foreach(int w in numbers)
+                    {
+                        suma *= w;
+                    }
+                }
+                return suma;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Tablice tablice = new Tablice();
+            tablice.wpisywanie();
+            tablice.wypisywanie();
+            Console.WriteLine(tablice.mat("*"));
+        }
+    }
+}
